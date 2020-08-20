5825ae28f6fc0fc4fa64ea0533925bf86094bd6b
gaggudeep <42181892+gaggudeep@users.noreply.github.com>
2020-08-20 20:09:23 +0530

    Add files via upload

---
+++ Assignment 1/palindromeLL.cpp
@@ -0,0 +1,14 @@
+ListNode* temp;
+bool isPalindrome(ListNode* head)
+{
+    temp = head;
+    return check(head);
+}
+
+bool check(ListNode* p)
+{
+    if (NULL == p) return true;
+    bool isPal = check(p->next) & (temp->val == p->val);
+    temp = temp->next;
+    return isPal;
+}
