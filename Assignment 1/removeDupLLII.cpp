26838e2508d8484c7924ed6a814e2e5ea295a03b
gaggudeep <42181892+gaggudeep@users.noreply.github.com>
2020-08-20 20:08:13 +0530

    Add files via upload

---
+++ Assignment 1/removeDupLLII.cpp
@@ -0,0 +1,16 @@
+ListNode* deleteDuplicates(ListNode* head)
+{
+        if (!head) return 0;
+        if (!head->next) return head;
+
+        int val = head->val;
+        ListNode* p = head->next;
+
+        if (p->val != val) {
+            head->next = deleteDuplicates(p);
+            return head;
+        } else {
+            while (p && p->val == val) p = p->next;
+            return deleteDuplicates(p);
+        }
+    }
