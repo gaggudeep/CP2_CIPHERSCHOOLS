74ee0a6044d493b6bedeeb12ca0f60bd631e81d5
gaggudeep <42181892+gaggudeep@users.noreply.github.com>
2020-08-25 13:44:02 +0530

    Add files via upload

---
+++ Assignment 1/searchBST.cpp
@@ -0,0 +1,6 @@
+TreeNode* searchBST(TreeNode* root, int val) {
+    while (root != nullptr && root->val != val) {
+      root = (root->val > val) ? root->left : root->right;
+    }
+    return root;
+}
