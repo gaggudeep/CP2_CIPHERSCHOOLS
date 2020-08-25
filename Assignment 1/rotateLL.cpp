c9a12e865f0d1de77c3e783b34957026fc9d7350
gaggudeep <42181892+gaggudeep@users.noreply.github.com>
2020-08-25 13:39:59 +0530

    Add files via upload

---
+++ Assignment 1/rotateLL.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(head == NULL || head->next == NULL||k==0) return head;
+
+        ListNode* node = head;
+        int size =1;
+
+        while(node->next != NULL)
+        {
+            size++;
+            node = node->next;
+        }
+
+        node->next=head;
+
+        k = k%size;
+
+        while(--size >= k)
+        {
+            node=node->next;
+        }
+
+        ListNode* first = node->next;
+        node->next=NULL;
+
+        return first;
+    }
+};
