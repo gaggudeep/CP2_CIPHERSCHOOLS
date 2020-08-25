06e9e983080cda59e9e7d24a8b6b3abdd839e502
gaggudeep <42181892+gaggudeep@users.noreply.github.com>
2020-08-25 13:41:43 +0530

    Add files via upload

---
+++ Assignment 1/tiltBT.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int findTilt(TreeNode* root) {
+        if(root == NULL) return 0;
+
+        int res = 0;
+
+        postorder(root, res);
+
+        return res;
+    }
+private:
+    int postorder(TreeNode* root, int& res){
+        if(root == NULL) return 0;
+
+        int leftsum= postorder(root->left,res);
+
+        int rightsum = postorder(root->right,res);
+
+        res += abs(leftsum - rightsum);
+
+        return leftsum + rightsum + root->val;
+
+    }
+};
