ad71778c92b80997c936895527546141861dfced
gaggudeep <42181892+gaggudeep@users.noreply.github.com>
2020-08-25 13:44:14 +0530

    Add files via upload

---
+++ Assignment 1/LCABST.cpp
@@ -0,0 +1,13 @@
+TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode* cur = root;
+        while (true) {
+            if (p -> val < cur -> val && q -> val < cur -> val) {
+                cur = cur -> left;
+            } else if (p -> val > cur -> val && q -> val > cur -> val) {
+                cur = cur -> right;
+            } else {
+                break;
+            }
+        }
+        return cur;
+    }
